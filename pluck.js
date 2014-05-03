b0ab74ca9e0e99200fb660cbcee2e11b9a02f726
tjmehta <tejesh.mehta@gmail.com>
2014-05-03 11:59:37 -0700

    pluck cleanup

--- pluck.js
+++ pluck.js
@@ -1,22 +1,16 @@
 var isObject = require('./isObject');
 
-// (obj, keys)
-// (obj, keys..)
-// (keys)(obj)
-// (keys..)(obj)
-module.exports = function () {
-  var args = Array.prototype.slice.call(arguments);
-  var obj, key;
-  if (isObject(args[0])) {
-    obj = args[0];
-    key = args[1];
-    return pluck(obj, key);
-  }
-  else {
-    key = args[0];
+// (obj, key)
+// (key)(obj)
+module.exports = function (obj, key) {
+  if (!isObject(obj)) {
+    key = obj;
     return function (obj) {
       return pluck(obj, key);
     };
+  }
+  else {
+    return pluck(obj, key);
   }
 };
 
