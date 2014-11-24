93bcc7d5e23581eb1ce08f230b58cb307b1bc609
johnmarinelli <jonmarinelli@hotmail.com>
2014-11-24 13:03:18 -0700

    fixed close parens for test

--- test/test-size.js
+++ test/test-size.js
@@ -9,29 +9,29 @@ var size = require('../size')
 
 describe('size', function() {
   it('should return 0 for empty array', function(done) {
-    expect(size([]).to.equal(0);
+    expect(size([]).to.equal(0));
     done();
   });
   it('should return 0 for empty map', function(done) {
-    expect(size({}).to.equal(0);
+    expect(size({}).to.equal(0));
     done();
   });
   it('should return 0 for empty string', function(done) {
-    expect(size("").to.equal(0);
+    expect(size("").to.equal(0));
     done();
   });
 
   
   it('should return 3 for [1, 2, 100]', function(done) {
-    expect(size([1, 2, 100]).to.equal(3);
+    expect(size([1, 2, 100]).to.equal(3));
     done();
   });
   it('should return 3 for { a: 1, b: 2, c: 100 }', function(done) {
-    expect(size({ a: 1, b: 2, c: 100}).to.equal(3);
+    expect(size({ a: 1, b: 2, c: 100}).to.equal(3));
     done();
   });
   it('should return 11 for "hello world"', function(done) {
-    expect(size("hello world").to.equal(11);
+    expect(size("hello world").to.equal(11));
     done();
   });
 });
