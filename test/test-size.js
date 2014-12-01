93d17521a129edd17729333f158942d8600373dc
johnmarinelli <jonmarinelli@hotmail.com>
2014-12-01 11:51:42 -0700

    added tests

--- test/test-size.js
+++ test/test-size.js
@@ -9,54 +9,57 @@ var size = require('../size')
 
 describe('size', function() {
   it('should return [0] for no args', function(done) {
-    expect(size()).to.equal([0]);
+    expect(size()).to.eql([ 
+      0 
+    ]);
     done();
   });
   
   it('should return [N] for a number with N digits', function(done) {
-    expect(size(1234)).to.equal([4]);
+    expect(size(1234)).to.eql([4]);
     done();
   });
   
   it('should return [N] for an object with N properties', function(done) {
     var obj = { foo: 'bar', fn: function() {}, 0: 100 };
-    expect(size(obj)).to.equal([3]);
+    expect(size(obj)).to.eql([3]);
     done();
   });
   
   it('should return [N] for an object with N properties', function(done) {
     var obj = { foo: 'bar', fn: function() {}, 0: 100 };
-    expect(size(obj)).to.equal([3]);
+    expect(size(obj)).to.eql([3]);
     done();
   });
 
   it('should return [0] for null', function(done) {
     var obj = { foo: 'bar', fn: function() {}, 0: 100 };
-    expect(size(null)).to.equal([0]);
+    expect(size(null)).to.eql([0]);
     done();
   });
   
   it('should return [0,0,0] for [null, undefined, Infinity]', function(done) {
-    expect(size([null, undefined, Infinity])).to.equal([0, 0, 0]);
+    expect(size([null, undefined, Infinity])).to.eql([0, 0, 0]);
     done();
   });
   
   it('should return [3] for [100]', function(done) {
-    expect(size([100]).to.equal([3]);
+    expect(size([100])).to.eql([3]);
     done();
   });
   
   it('should return [1, 2, 3] for [1, 22, 333]', function(done) {
-    expect(size([1, 22, 333]).to.equal([1,2,3]);
+    expect(size([1, 22, 333])).to.eql([1,2,3]);
     done();
   });
   
   it('should return [1, 2, 3] for [ { foo: "bar" }, "hi", [1,2,3] ]', function(done) {
-    expect(size([ { foo: 'bar' }, 'hi', [1, 2, 3] ])).to.equal([1, 2, 3]);
+    expect(size([ { foo: 'bar' }, 'hi', [1, 2, 3] ])).to.eql([ 1, 2, 3 ]);
     done();
   });  
 
-  it('should return [ [1], [2], [1, 1, 1] ] for [ { foo: "bar" }, "hi", [1,2,3] ].map(size)') {
-    expect([{ foo: 'bar' }, 'hi', [1,2,3]].map(size)).to.equal([ [1], [2], [1, 1, 1] ]);
-  }
+  it('should return [ [1], [2], [1, 1, 1] ] for [ { foo: "bar" }, "hi", [1,2,3] ].map(size)', function(done) {
+    expect([{ foo: 'bar' }, 'hi', [1,2,3]].map(size)).to.eql([ [1], [2], [1, 1, 1] ]);
+    done();
+  });
 });
