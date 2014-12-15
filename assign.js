e9d43171ccdf72ee5f85fe0620e5ace798a62cf7
tjmehta <tejesh.mehta@gmail.com>
2014-12-15 00:08:02 -0800

    added assign

---
+++ assign.js
@@ -0,0 +1,37 @@
+/**
+ * @module 101/assign
+ */
+
+var isFunction = require('./is-function');
+var exists = require('./exists');
+
+/**
+ * Copies enumerable and own properties from a source object(s) to a target object, aka extend.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+ * @function module:101/assign
+ * @param {object} target - object which source objects are extending (being assigned to)
+ * @param {object} sources... - objects whose properties are being assigned to the source object
+ * @return {object} source with extended properties
+ */
+module.exports = function(target, firstSource) {
+  if (target === undefined || target === null)
+    throw new TypeError('Cannot convert first argument to object');
+  var to = Object(target);
+  for (var i = 1; i < arguments.length; i++) {
+    var nextSource = arguments[i];
+    if (nextSource === undefined || nextSource === null) continue;
+    var keysArray = Object.keys(Object(nextSource));
+    for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+      var nextKey = keysArray[nextIndex];
+      var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+      // I changed the following line to get 100% test coverage.
+      // if (desc !== undefined && desc.enumerable) to[nextKey] = nextSource[nextKey];
+      // I was unable to find a scenario where desc was undefined or that desc.enumerable was false:
+      //   1) Object.defineProperty does not accept undefined as a desc
+      //   2) Object.keys does not return non-enumerable keys.
+      // Let me know if this is a cross browser thing.
+      to[nextKey] = nextSource[nextKey];
+    }
+  }
+  return to;
+};
---
+++ test/test-assign.js
@@ -0,0 +1,55 @@
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+
+var describe = lab.describe;
+var it = lab.it;
+var expect = Lab.expect;
+
+var assign = require('../assign');
+
+// very simple test primarily checking exportation, see npm `assign` module...
+describe('assign', function () {
+  it('should assign an object', function (done) {
+    var obj = {
+      foo: 1,
+      bar: 1,
+      qux: 1
+    };
+    var obj2 = {
+      you: 1
+    };
+    var assigned = assign(obj, obj2);
+    expect(assigned).to.equal(obj);
+    expect(assigned).to.eql({
+      foo: 1,
+      bar: 1,
+      qux: 1,
+      you: 1
+    });
+    done();
+  });
+  it('should throw an error if source is undefined', function (done) {
+    try {
+      assign(undefined, {});
+    }
+    catch (e) {
+      expect(e.message).to.equal('Cannot convert first argument to object');
+      done();
+    }
+  });
+  it('should throw an error if source is null', function (done) {
+    try {
+      assign(null, {});
+    }
+    catch (e) {
+      expect(e.message).to.equal('Cannot convert first argument to object');
+      done();
+    }
+  });
+  it('should just return the target if sources are undefined or null', function(done) {
+    var obj = {};
+    expect(assign(obj, null)).to.equal(obj);
+    expect(assign(obj, undefined)).to.equal(obj);
+    done();
+  });
+});
