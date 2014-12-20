1209f3fd191c74902476f6cb82a07d1e257bcd27
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2014-12-21 00:46:17 +0100

    Remove files dependent on external modules from the ES6 build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
+var basename = require('basename');
+var clone = require('clone');
 var gulp = require('gulp');
 var replace = require('gulp-replace');
+var stripComments = require('strip-comments');
+var stream = require('through2').obj;
+
+var findIndex = require('./find-index');
 
 
 //
@@ -24,11 +30,50 @@ gulp.task('default', ['es6']);
 //
 
 var requireCall = /^var ([^ ]+) = require\('(\.\/[a-z-]+)'\);$/mg;
-var exportsAssignment = /^module.exports = /mg;
+var exportsAssignment = /^module\.exports = /mg;
 
 gulp.task('es6', function () {
+  var files = [];
+
   return gulp.src('*.js')
+
+    // Do the simple search'n'replace.
     .pipe(replace(requireCall, "import $1 from '$2';"))
     .pipe(replace(exportsAssignment, 'export default '))
+
+    // Skip a file if the string 'require(' or 'module.exports' is still there.
+    .pipe(stream(function (file, encoding, done) {
+      var rawContents = stripComments(file.contents.toString());
+      if (/(^|\s)require\s*\(/.test(rawContents)) return done();
+      if (/(^|\s)module\.exports[^w]/.test(rawContents)) return done();
+
+      files.push(file);
+      done();
+
+    // Find modules which depend on skipped ones and remove them from the stream.
+    }, function (done) {
+      var safeFiles = (function removeUnrooted (files) {
+        var availableModules = files.map(function (file) { return basename(file.path); });
+        var unrootedIndex = findIndex(files, function isUnrooted (file) {
+          var match;
+          var importStatement = /import [^ ]+ from '.\/([a-z-]+)';/g;
+          var contents = file.contents.toString();
+          while ((match = importStatement.exec(contents))) {
+            if (availableModules.indexOf(match[1]) == -1) return true;
+          }
+        });
+
+        if (unrootedIndex >= 0) {
+          files.splice(unrootedIndex, 1);
+          return removeUnrooted(clone(files));
+        }
+        else return clone(files);
+      })(files);
+
+      safeFiles.forEach(this.push.bind(this));
+      done();
+    }))
+
+    // Save the processed files.
     .pipe(gulp.dest('es6'));
 });
