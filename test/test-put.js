1a8850d050530bf68ccb5e03c9dc7d67f7bba805
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-03-09 21:39:17 +0100

    Update spec to mirror behavior of `set`

--- test/test-put.js
+++ test/test-put.js
@@ -4,8 +4,9 @@ var lab = exports.lab = Lab.script();
 var describe = lab.describe;
 var it = lab.it;
 var expect = Lab.expect;
-var clone = require('clone');
+var clone = require('../clone');
 var put = require('../put');
+var set = require('../set');
 
 describe('put', function () {
   it('should put a value at a key on a new object', function(done) {
@@ -61,6 +62,61 @@ describe('put', function () {
     done();
   });
 
+  it('should put a key-value set on a new object', function(done) {
+    var obj = {
+      foo: 1,
+      bar: 1,
+      qux: 1
+    };
+
+    var key = 'bar';
+    var val = 100;
+    var putObj = set({}, key, val);
+
+    var original = clone(obj);
+
+    var expected = clone(obj);
+    expected[key] = val;
+
+    expect(put(obj, putObj)).to.eql(expected);
+    expect(obj).to.deep.equal(original); // original not modified
+    done();
+  });
+
+  it('should put a key-value set when used with array functions', function(done) {
+    var objs = [
+      {
+        bar: 1
+      },
+      {
+        foo: 2,
+        bar: 2,
+        qux: 2
+      },
+      {
+        foo: 3,
+        bar: 3,
+        koo: 3,
+        goo: 3
+      }
+    ];
+
+    var key = 'bar';
+    var val = 100;
+    var putObj = set({}, key, val);
+
+    var originals = clone(objs);
+
+    var expected = objs.map(function (obj) {
+      var out = clone(obj);
+      out[key] = val;
+      return out;
+    });
+    expect(objs.map(put(putObj))).to.eql(expected);
+    expect(objs).to.eql(originals); // original not modified
+    done();
+  });
+
   it("shouldn't change the type of the object", function(done) {
     var obj = new Date();
 
@@ -77,18 +133,20 @@ describe('put', function () {
   });
 
   describe('errors', function() {
-    it('should error when given two args other than {string|number}, val', function (done) {
+    it('should error when given two args other than (key, value) or (obj, putObj)', function (done) {
       try {put(/whatever/, /whatever/);}
       catch (err) {expect(err).to.be.ok;}
       done();
     });
 
-    it('should error when given no args, one arg, or more than three args', function (done) {
+    it('should error when given no args', function (done) {
       try {put();}
       catch (err) {expect(err).to.be.ok;}
-      try {put(1);}
-      catch (err) {expect(err).to.be.ok;}
-      try {put(1, 3, 3, 4);}
+      done();
+    });
+
+    it('should error when one arg other than putObj', function (done) {
+      try {put("anything");}
       catch (err) {expect(err).to.be.ok;}
       done();
     });
