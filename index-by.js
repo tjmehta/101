04dca5a4319982e028e38087a4e9d45ac98b7980
tjmehta <tejesh.mehta@gmail.com>
2015-05-09 02:30:46 -0700

    index by tweaks

--- index-by.js
+++ index-by.js
@@ -1,39 +1,40 @@
 /**
- * @module 101/indexBy
+ * @module 101/index-by
  */
 
 var isString = require('./is-string');
+var isFunction = require('./is-function');
+var pluck = require('./pluck');
 
 /**
  * Hashes an array of objects based on the value of a provided common key.
  * @function module:101/index-by
- * @param {array|function|string} [obj] - object from which the value is plucked
- * @param {string|function} indexer - the common key or function which accepts and object and returns an index key
- * @return {obj|function} An object whose keys are the values of their common key and values are the original values. Or a partial funciton which applies the passed indexer to each object.
+ * @param {array|function|string} arr|indexer - array of objects to be indexed
+ * @param {string|function} indexer - the common keypath or function which accepts an object and returns an index key
+ * @return {obj|function} indexed|indexByPartial An object whose keys are the values of their common key and values are the original values.
+ *     Or a partial function which applies the passed indexer to each object.
 */
-module.exports = function() {
-  if (arguments.length == 1) {
-    return indexBy(arguments[0]); 
-  }
-  else if (arguments.length == 2) {
-    return arguments[0].reduce(indexBy(arguments[1]), {});
+module.exports = function(arrOrIndexer, indexer) {
+  var arr;
+  if (Array.isArray(arrOrIndexer)) {
+    arr = arrOrIndexer;
+    return arr.reduce(indexBy(indexer), {});
   }
   else {
-    return {};
+    indexer = arrOrIndexer;
+    if (!isString(indexer) && !isFunction(indexer)) {
+      throw new TypeError('indexer must be a string or function');
+    }
+    return indexBy(indexer);
   }
 }
 
 function indexBy (indexer) {
   if (isString(indexer)) {
-    return function(prev, cur, index, arr) {
-      prev[cur[indexer]] = cur;
-      return prev;
-    };
+    indexer = pluck(indexer);
   }
-  else {
-    return function(prev, cur, index, arr) {
-      prev[indexer(cur)] = cur;
-      return prev;
-    };
-  }
+  return function(prev, cur, index, arr) {
+    prev[indexer(cur)] = cur;
+    return prev;
+  };
 }
