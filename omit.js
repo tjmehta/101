5375ba31e89e6d8d5a92fb61ebe81a5ee907f5c4
Casey Flynn <cflynn.us@gmail.com>
2015-12-29 22:58:39 -0800

    Refactor of omit

--- omit.js
+++ omit.js
@@ -3,41 +3,65 @@
  */
 
 var isObject = require('./is-object');
-var clone = require('./clone');
+var isString = require('./is-string');
 
 /**
  * Returns a new object without the specified keys.
  * When only keys are specified omit returns a partial-function which accepts obj.
  * @function module:101/omit
- * @param {object} [obj] - object whose keys are omited
- * @param {string|array} keys... - keys which will be dropped from obj (can be specifieds as args (strings and/or arrays)
- * @return {object|function} Object without the specified keys from the original obj or Partial-function omit (which accepts obj) and returns an object
+ * @param {Object} [obj] - object whose keys are omited
+ * @param {String|Array} keys... - keys which will be dropped from obj (can be specifieds as args
+ *   (strings and/or arrays)
+ * @return {Object|Function} Object without the specified keys from the original obj or
+ *   Partial-function omit (which accepts obj) and returns an object
  */
 module.exports = function () {
-  var args = Array.prototype.slice.call(arguments);
-  if (isObject(args[0])) {
-    var obj = args.shift();
-    return omit(obj, args);
+  var omitKeysArray;
+  var omitObject;
+
+  if (arguments.length === 0) {
+    return function (obj) { return obj; }
   }
-  else {
+
+  if (arguments.length <= 1) {
+    // Return partial function
+    // Only keys provided, will return a function that takes an abject as an argument omits the keys
+    if (isObject(arguments[0])) {
+      return arguments[0];
+    }
+
+    if (isString(arguments[0])) {
+      omitKeysArray = [arguments[0]];
+    } else if (!Array.isArray(arguments[0])) {
+      throw new Error('101/omit invalid arguments');
+    } else {
+      omitKeysArray = arguments[0];
+    }
+
     return function (obj) {
-      return omit(obj, args);
-    };
+      return omit(obj, omitKeysArray);
+    }
+  } else if (arguments.length > 1) {
+    if (!isObject(arguments[0])) {
+      throw new Error('101/omit invalid arguments');
+    }
+    omitKeysArray = Array.prototype.slice.call(arguments);
+    omitObject = omitKeysArray.shift();
+    omitKeysArray = omitKeysArray.reduce(function (prev, cur) { return prev.concat(cur); }, []);
+    return omit(omitObject, omitKeysArray);
   }
 };
 
-function omit (obj, args) {
-  var keys = [];
-  args.forEach(function (key) {
-    keys = keys.concat(key);
+/**
+ * @param {Object} obj
+ * @param {Array} omitKeys
+ * @return Object
+ */
+function omit (obj, omitKeys) {
+  var returnObject = {};
+  Object.keys(obj).forEach(function (key) {
+    if (~omitKeys.indexOf(key)) { return; }
+    returnObject[key] = obj[key];
   });
-  var out = clone(obj);
-  keys.forEach(remove(out));
-  return out;
-}
-
-function remove (obj) {
-  return function (key) {
-    delete obj[key];
-  };
+  return returnObject;
 }
