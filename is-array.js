d163673074ed764d2bfe9512c044992b48027599
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-08-25 10:05:39 +0800

    Create is-array.js

---
+++ is-array.js
@@ -0,0 +1,17 @@
+/**
+ * @module 101/is-array
+ */
+
+/**
+ * Checks if a value is an array.
+ * @function module:101/is-array
+ * @param {*} val - value checked to be an array
+ * @return {boolean} Whether the value is a function or not
+ */
+module.exports = Array.prototype.isArray || isArray;
+
+var toString = Object.prototype.toString;
+
+function isArray (v) {
+  return !!v && '[object Array]' === toString.call(v);
+}
